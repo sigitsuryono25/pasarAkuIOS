61ec3bd819df5cfa7e60880c04b83a17b2304d66
Sigit Suryono <sigitharsy25@gmail.com>
2025-09-01 20:02:56 +0700

    Initial Commit

---
+++ PasarAkuTests/PasarAkuTests.swift
@@ -0,0 +1,17 @@
+//
+//  PasarAkuTests.swift
+//  PasarAkuTests
+//
+//  Created by Sigit Sur on 01/09/25.
+//
+
+import Testing
+@testable import PasarAku
+
+struct PasarAkuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
